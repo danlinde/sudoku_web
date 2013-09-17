be17896632694226420c2f71963857e73f414a5b
DanL <ddlinde@hotmail.com>
2013-09-17 14:24:13 +0100

    add partials

--- sudoku.rb
+++ sudoku.rb
@@ -1,8 +1,13 @@
 require 'sinatra'
+require 'sinatra/partial'
 require_relative './lib/sudoku'
 require_relative './helpers/application'
 
+set :partial_template_engine, :erb
 enable :sessions
+
+
+
 
 def random_sudoku
     seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
